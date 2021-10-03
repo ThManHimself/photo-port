f6705ad07fde0d8a67e0d0e40b7683dd8880a434
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-10-03 16:41:53 -0500

    created <Contact> tests

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -47,7 +47,7 @@ function ContactForm() {
     // JSX
     return(
         <section>
-            <h1>Contact me</h1>
+            <h1 data-testid="h1tag">Contact me</h1>
             <form id="contact-form" onSubmit={ handleSubmit } >
                 <div>
                     <label htmlFor="name">Name:</label>
@@ -66,7 +66,7 @@ function ContactForm() {
                         <p className="error-text">{ errorMessage }</p>
                     </div>
                 ) }
-                <button type="submit">Submit</button>
+                <button type="submit" data-testid="submitBtn">Submit</button>
             </form>
         </section>
     );
