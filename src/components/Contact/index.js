57b7078f1166108f903e7562468d2bd74571176d
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-10-03 14:12:33 -0500

    Can now manip., submit, and validate contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ContactForm() {
+    // event.preventDefault - (goes in event listener)
+
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+
+    // sync the internal state of the component formState with the user input from the DOM
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value });
+    }
+
+    console.log(formState);
 
     // JSX
     return(
@@ -9,15 +20,15 @@ function ContactForm() {
             <form id="contact-form">
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" defaultValue={ name } onChange={ handleChange } name="name" />
                 </div>
                 <div>
                     <label htmlFor="email">Email Address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" defaultValue={ email } onChange={ handleChange } name="email" />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea name="message" defaultValue={ message } onChange={ handleChange } rows="5" />
                 </div>
                 <button type="submit">Submit</button>
             </form>
