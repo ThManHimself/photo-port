a77cba085958a9bce7af2a44f7ce12b45cd2a7f5
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-10-06 18:57:19 -0500

    minor changes while referenceing

--- src/App.js
+++ src/App.js
@@ -5,22 +5,14 @@ import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
 function App() {
-
 	const [categories] = useState([
-        { 
-			name: 'commercial', 
-			description: "Photos of grocery stores, food trucks, and other commercial projects" 
-		},
+        { name: 'commercial', description: "Photos of grocery stores, food trucks, and other commercial projects" },
         { name: 'portraits', description: "Portraits of people in my life" },
         { name: 'food', description: "Delicious delicacies" },
         { name: 'landscape', description: "Fields, farmhouses, waterfalls, and the beauty of nature" }
     ]);
-
 	const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
-
 	const [contactSelected, setContactSelected] = useState(false);
-
 
 	return (
 		<div>
--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
-function Gallery(props) {
-    const { currentCategory } = props;
+function Gallery({ currentCategory }) {
     return (
         <section>
             <h1 data-testid="h1tag">{ capitalizeFirstLetter(currentCategory.name) }</h1>
